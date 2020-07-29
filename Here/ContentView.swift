2efd2730715e6a5e5586064fea8162316ebf18a2
ttdp <tiantong.dev@gmail.com>
2020-07-29 20:43:47 +0800

    Initial Commit

---
+++ Here/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Here
+//
+//  Created by TIAN TONG on 7/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Here/HereApp.swift
@@ -0,0 +1,17 @@
+//
+//  HereApp.swift
+//  Here
+//
+//  Created by TIAN TONG on 7/29/20.
+//
+
+import SwiftUI
+
+@main
+struct HereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
